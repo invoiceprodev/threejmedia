c0b3a3c1223f18795929ef2f3e8b417f87e3db3d
pssjmokoena <invoiceprodev@gmail.com>
2026-03-14 14:08:34 +0200

    Use Cloudinary asset URLs for landing images

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -6,6 +6,15 @@ import localPortfolioFour from "../../images/portfolio-4.png";
 import localPortfolioFive from "../../images/portfolio-5.png";
 import { env } from "./env";
 
+const cloudinaryAssetUrls = {
+  logo: "https://res.cloudinary.com/dwqrz9shj/image/upload/v1773489792/threejmedia_logo.png",
+  portfolioOne: "https://res.cloudinary.com/dwqrz9shj/image/upload/v1773489824/portfolio-1.png",
+  portfolioTwo: "https://res.cloudinary.com/dwqrz9shj/image/upload/v1773489837/portfolio-2.png",
+  portfolioThree: "https://res.cloudinary.com/dwqrz9shj/image/upload/v1773489852/portfolio-3.png",
+  portfolioFour: "https://res.cloudinary.com/dwqrz9shj/image/upload/v1773489866/portfolio-4.png",
+  portfolioFive: "https://res.cloudinary.com/dwqrz9shj/image/upload/v1773489877/portfolio-1-2.png",
+};
+
 function getCloudinaryBaseUrl() {
   if (!env.cloudinary.cloudName) {
     return "";
@@ -30,10 +39,20 @@ function resolveHostedImage(filename: string, fallback: string) {
 }
 
 export const imageAssets = {
-  logo: resolveHostedImage("threejmedia_logo.png", localLogo),
-  portfolioOne: resolveHostedImage("portfolio-1.png", localPortfolioOne),
-  portfolioTwo: resolveHostedImage("portfolio-2.png", localPortfolioTwo),
-  portfolioThree: resolveHostedImage("portfolio-3.png", localPortfolioThree),
-  portfolioFour: resolveHostedImage("portfolio-4.png", localPortfolioFour),
-  portfolioFive: resolveHostedImage("portfolio-5.png", localPortfolioFive),
+  logo: env.cloudinary.cloudName ? cloudinaryAssetUrls.logo : resolveHostedImage("threejmedia_logo.png", localLogo),
+  portfolioOne: env.cloudinary.cloudName
+    ? cloudinaryAssetUrls.portfolioOne
+    : resolveHostedImage("portfolio-1.png", localPortfolioOne),
+  portfolioTwo: env.cloudinary.cloudName
+    ? cloudinaryAssetUrls.portfolioTwo
+    : resolveHostedImage("portfolio-2.png", localPortfolioTwo),
+  portfolioThree: env.cloudinary.cloudName
+    ? cloudinaryAssetUrls.portfolioThree
+    : resolveHostedImage("portfolio-3.png", localPortfolioThree),
+  portfolioFour: env.cloudinary.cloudName
+    ? cloudinaryAssetUrls.portfolioFour
+    : resolveHostedImage("portfolio-4.png", localPortfolioFour),
+  portfolioFive: env.cloudinary.cloudName
+    ? cloudinaryAssetUrls.portfolioFive
+    : resolveHostedImage("portfolio-5.png", localPortfolioFive),
 };
